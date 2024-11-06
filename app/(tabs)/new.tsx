23bf2ec11df886f279607f18406c107f59a1562e
saulamsal <saulamsal@gmail.com>
2024-11-06 17:49:11 -0500

    wip

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -28,6 +28,8 @@ interface ComingSoonItem {
     subText: string;
     rating: string;
     logo: string;
+    logoWidth: number;
+    logoHeight: number;
 }
 
 const COMING_SOON_DATA: ComingSoonItem[] = [
@@ -39,7 +41,9 @@ const COMING_SOON_DATA: ComingSoonItem[] = [
         imageUrl: 'https://occ-0-2430-2433.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSWRinEsQOaPuYqcT8MP4lkknrc5czNm9qgpVzfBrl7maTIufi_VLNohrkfAyT4zPnHfos7z0-PMaq-cjPP8qI4fmsmo7F4nRO6M.jpg?r=77a',
         description: 'Jake Paul battles Mike Tyson as they headline this must-see boxing mega-event streaming live in five languages from AT&T Stadium in Arlington, Texas.',
         rating: 'TV-14',
-        logo: 'https://i.imgur.com/T9zM4Ro.png'
+        logo: 'https://i.imgur.com/T9zM4Ro.png',
+        logoWidth: 155,
+        logoHeight: 80
     },
 
     {
@@ -53,10 +57,15 @@ const COMING_SOON_DATA: ComingSoonItem[] = [
     },
     {
         id: '1',
-        title: 'Outer Banks',
+        title: 'The Madness',
         date: 'NOV 7',
-        description: 'Season 4: Part 2 of the popular teen mystery drama continues the adventures of the Pogues in their treasure-hunting escapades.',
+        subText: 'Limited Series Coming November 28',
+        imageUrl: 'https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABS9bCR4F6Js4srd5OilhZjU7lDN1fPubypyab5erIbx1_2z-p82uS8v1jrQw2g4afqaVLtGLq-ZLDQDrIp__lui88gHXmWogeu-hG8_zGTFNCH963EFK89IlVeuHwwalhtHGaA.webp?r=0c3',
+        description: 'After a media pundit stumbles upon a dead body deep in the Poconos woods, he finds himself framed for the murder of a notorious white supremacist.',
         rating: 'TV-MA',
+        logo: 'https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABZPHktPl9bC4Ag5omE8ZnpGG0bCUfOZDbuLZ3QfDBSlVHWNnAdvF5IHX7SYwFINJFcZgRg9DEojRH0E-mTt_zu1ddC_Oc-NZ7Ys1JJrD4VJm_WE--1AopS6Bde3Nq0YD7KADj9v0beVHuvxKH-FQ8ySCm8mtteFTsr4aVGX2UAXpz9h_2xRnTA.png?r=469',
+        logoWidth: 190,
+        logoHeight: 40
     },
     {
         id: '2',
@@ -186,7 +195,7 @@ export default function NewScreen() {
                     <View style={{ gap: 6, }}>
                         <Image
                             source={{ uri: item.logo }}
-                            style={{ width: 155, height: 80, marginRight: 4 }}
+                            style={{ width: item.logoWidth, height: item.logoHeight, marginRight: 4, marginLeft: 12 }}
                         />
 
                     </View>
